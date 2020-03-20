3060c8833150fb03593ad2d5fab74279d48c3f47
hxy112292 <983771687@qq.com>
2020-03-20 07:26:49 -0500

    修改blog下的资源路径

---
+++ js/login-blog.js
@@ -0,0 +1,52 @@
+window.onload = createLoginHtml();
+
+function createLoginHtml() {
+
+    const loginHtml = "<div class=\"form-popup\" id=\"loginWindow\">\n" +
+        "        <div class=\"closeButton\">\n" +
+        "            <img src=\"../images/closeButton.png\" onclick=\"closeLoginForm()\"/>\n" +
+        "        </div>\n" +
+        "        <div class=\"container\">\n" +
+        "            <div class=\"content\">\n" +
+        "                <div class=\"oneElementCenter\">\n" +
+        "                    <img src=\"../images/logo.png\" alt=\"\"/>\n" +
+        "                    <h2>CENTRO AUGUSTO MIJARES</h2>\n" +
+        "                </div>\n" +
+        "                <form id=\"loginForm\">\n" +
+        "                    <div>\n" +
+        "                        <h3>Iniciar Sesion</h3>\n" +
+        "                        <label for=\"loginUsername\"></label><input type=\"text\" id=\"loginUsername\" placeholder=\"Numbre de Usuario o Correo\"/>\n" +
+        "                        <label for=\"loginPassword\"></label><input type=\"password\" id=\"loginPassword\" placeholder=\"Contraseña\"/>\n" +
+        "                    </div>\n" +
+        "                    <div>\n" +
+        "                        <a href=\"../dashboard.html\" onclick=\"login()\">Entrar</a>\n" +
+        "                    </div>\n" +
+        "                </form>\n" +
+        "            </div>\n" +
+        "        </div>\n" +
+        "    </div>";
+
+    const loginPage = document.createElement("div");
+    loginPage.innerHTML = loginHtml;
+    document.getElementById("loginPage").appendChild(loginPage);
+}
+
+
+function openLoginForm() {
+    document.getElementById("loginWindow").style.display = "block";
+}
+
+function closeLoginForm() {
+    document.getElementById("loginWindow").style.display = "none";
+}
+
+function login() {
+    localStorage.setItem("uid", "1");
+}
+
+function signOut() {
+    localStorage.removeItem("uid");
+    location.reload();
+}
+
+
---
+++ js/signup-blog.js
@@ -0,0 +1,58 @@
+
+window.onload = createSignUpHtml();
+
+function createSignUpHtml() {
+    const signupHtml = "    <div class=\"form-popup\" id=\"signupWindow\">\n" +
+        "        <div class=\"closeButton\">\n" +
+        "            <img src=\"../images/closeButton.png\" onclick=\"closeSignupForm()\"/>\n" +
+        "        </div>\n" +
+        "        <div class=\"container\">\n" +
+        "            <div class=\"content\">\n" +
+        "                <div class=\"oneElementCenter\">\n" +
+        "                    <img src=\"../images/logo.png\" alt=\"\" style=\"width: 80px; height: 80px\"/>\n" +
+        "                    <h3>Registro</h3>\n" +
+        "                </div>\n" +
+        "                <div class=\"row\">\n" +
+        "                    <div class=\"col-12-left\">\n" +
+        "                        <form id=\"signupForm\">\n" +
+        "                            <div class=\"row\">\n" +
+        "                                <label for=\"signupUsername\"></label><input type=\"text\" id=\"signupUsername\" placeholder=\"Numbre\"/>\n" +
+        "                                <label for=\"correo\"></label><input type=\"text\" id=\"correo\" placeholder=\"Correo\"/>\n" +
+        "                            </div>\n" +
+        "                            <div class=\"row\">\n" +
+        "                                <label for=\"signupPassword\"></label><input type=\"password\" id=\"signupPassword\" placeholder=\"Contraseña\"/>\n" +
+        "                                <label for=\"signupPassword2\"></label><input type=\"password\" id=\"signupPassword2\" placeholder=\"Repeater Contraseña\"/>\n" +
+        "                            </div>\n" +
+        "                            <div class=\"row\">\n" +
+        "                                <label for=\"direction\"></label><input type=\"text\" id=\"direction\" placeholder=\"Direccion\"/>\n" +
+        "                            </div>\n" +
+        "                            <div>\n" +
+        "                                <a href=\"../dashboard.html\" onclick=\"signup()\">Guardar</a>\n" +
+        "                            </div>\n" +
+        "                        </form>\n" +
+        "                    </div>\n" +
+        "                    <div class=\"col-12-right\">\n" +
+        "                        <img src=\"../images/logo.png\" alt=\"\"/>\n" +
+        "                        <h2>CENTRO AUGUSTO MIJARES</h2>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "        </div>\n" +
+        "    </div>"
+
+    const signupPage = document.createElement("div");
+    signupPage.innerHTML = signupHtml;
+    document.getElementById("signupPage").appendChild(signupPage);
+}
+
+function openSignupForm() {
+    document.getElementById("signupWindow").style.display = "block";
+}
+
+function closeSignupForm() {
+    document.getElementById("signupWindow").style.display = "none";
+}
+
+function signup() {
+    localStorage.setItem("uid", "1");
+}
