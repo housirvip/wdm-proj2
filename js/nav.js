37eed6fdc1c963e82f71ccae793ee0e6ef76f866
housirvip <housirvip@outlook.com>
2020-04-22 22:56:35 -0500

    change to laravel

--- js/nav.js
+++
@@ -1,8 +0,0 @@
-function hideIcon() {
-    var x = document.getElementById("header");
-    if (x.className === "header") {
-        x.className += " responsive";
-    } else {
-        x.className = "header";
-    }
-}
